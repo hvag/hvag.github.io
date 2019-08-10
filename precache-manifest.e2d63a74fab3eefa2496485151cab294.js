63e8dc4b5a3d76dbdd9e001a4d03eb96b1b161c4
hmashaw <hmashaw@gmail.com>
2019-08-10 16:52:51 -0400

    Updates

---
+++ precache-manifest.e2d63a74fab3eefa2496485151cab294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c751e18937e96567350cf0ca5674303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8419b481f71987e31f22",
+    "url": "/static/css/main.904d04df.chunk.css"
+  },
+  {
+    "revision": "6256b6a1dcfc4c136321",
+    "url": "/static/js/2.372265ab.chunk.js"
+  },
+  {
+    "revision": "8419b481f71987e31f22",
+    "url": "/static/js/main.36994218.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
